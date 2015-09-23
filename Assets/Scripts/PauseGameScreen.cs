18d1523baea0e6619f716c14dafade364797cf7d
eranshac <eranmatematika@gmail.com>
2015-09-23 11:31:05 +0300

    w

--- Assets/Scripts/PauseGameScreen.cs
+++ Assets/Scripts/PauseGameScreen.cs
@@ -34,11 +34,15 @@ public class PauseGameScreen : MonoBehaviour {
 	}
 
 	public void PauseTheGame(){
+		VibrationManager.Vibrate(40);
+
 		Time.timeScale=0;
 
 	}
 
 	public void ToMainMenu(){
+		VibrationManager.Vibrate(40);
+
 		Time.timeScale=1;
 		
 	}
